dd0f83a0e896608d992ce29cee2a1de815d20668
Maxisthemoose <oriontothemax@gmail.com>
2022-06-19 12:04:55 -0700

    knight moves fixed

--- Pieces/Knight.js
+++ Pieces/Knight.js
@@ -11,8 +11,12 @@ class Knight extends Piece {
     const arrOfMoves = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]];
     for (const m of arrOfMoves) {
       const tempMove = [this.x + m[0], this.y + m[1]];
+
+      const pieceInLocation = Util.pieceAtCords(m, pieces);
+
       if (tempMove[0] > 7 || tempMove[0] < 0 || tempMove[1] > 7 || tempMove[1] < 0) continue;
-      else moves.push(tempMove);
+      else if (pieceInLocation !== undefined && pieceInLocation.color !== this.color) moves.push(tempMove);
+      else if (pieceInLocation === undefined) moves.push(tempMove);
     }
     return moves;
   }
